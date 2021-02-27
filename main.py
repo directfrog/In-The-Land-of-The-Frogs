5b28f68a6fdc3d992888da38ea873ca668322116
directfrog <directfrog@gmail.com>
2021-02-27 20:50:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,15 @@ jumping = pygame.image.load(jumping_img_load).convert()
 jumping.set_colorkey((255, 255, 255))
 jumping = pygame.transform.scale(jumping, (64, 64))
 
+d_img_load = os.path.join(os.path.dirname(os.path.abspath(__file__)), "d.png")
+d_img = pygame.image.load(d_img_load)
+d_img = pygame.transform.scale(d_img, (100, 100))
+e_img_load = os.path.join(os.path.dirname(os.path.abspath(__file__)), "e.png")
+e_img = pygame.image.load(e_img_load)
+e_img = pygame.transform.scale(e_img, (100, 100))
+a_img_load = os.path.join(os.path.dirname(os.path.abspath(__file__)), "a.png")
+a_img = pygame.image.load(a_img_load)
+a_img = pygame.transform.scale(a_img, (100, 100))
 
 idle_image_load  = os.path.join(os.path.dirname(os.path.abspath(__file__)), "idle.png")
 idle = pygame.image.load(idle_image_load).convert()
@@ -48,29 +57,6 @@ def ran(x, y):
     rand = random.randint(x, y)
     return ran(x, y) if rand == 0 else rand
 
-rock_index = 0
-
-def append_rocks():
-    global rock_index
-    random_choice = random.randint(1, 2)
-    if random_choice == 1:
-        x = random.randint(0, screen_width)
-        y = 0
-        if x > screen_width/2:
-            vel = [ran(-1, 3), ran(1, 3)]
-        elif x < screen_width/2:
-            vel = [ran(-1, 3), ran(1, 3)]
-    if random_choice == 2:
-        x = 0
-        y = ran(0, screen_height)
-        if y > screen_height/2:
-            vel = [ran(1, 3), ran(1, 3)]
-        elif y < screen_height/2:
-            vel = [ran(1, 3), ran(-3, -1)]
-    rock = [x, y, vel, rock_index]
-    rock_index += 1
-    return rock
-
 top_rect = pygame.Rect(0, -25, 400, 1)
 bottom_rect = pygame.Rect(0, 625, 400, 1)
 left_rect = pygame.Rect(-20, 0, 1, 600)
@@ -78,12 +64,35 @@ right_rect = pygame.Rect(420, 1, 1, 600)
 
 data = []
 
+def make_int(val):
+    if val < 0:
+        val = int(val+0.5)
+    elif val > 0:
+        val = int(val-0.5)
+    return val
+
+def generate_new():
+    ran_x = random.randint(20, screen_width-10)
+    ran_y = random.randint(10, 100)
+
+    x_vel = ran(-1, 1)
+    y_vel = ran(1, 3)
+    ##makes the vels ints###
+    v_vel = make_int(x_vel)
+    y_vel = make_int(y_vel)
+    if y_vel == 0:
+        y_vel = random.randint(1, 2)
+    return ran_x, ran_y, x_vel, y_vel
+
 for x in range(3):
-    new_rock = data.append(append_rocks())
+    ran_x, ran_y, x_vel, y_vel = generate_new()
+    data.append([ran_x, ran_y, x_vel, y_vel])
 
-
-while running:
-    screen.fill((0, 235, 155))
+game_over = False
+running = True
+fill = (0, 235, 155)
+while True:
+    screen.fill(fill)
 
     velocity = [0,0]
     if frog_rect.y > 500:
@@ -104,7 +113,8 @@ while running:
         action = idle
     y_check = frog_rect.y
 
-    screen.blit(action, (frog_rect.x, frog_rect.y))
+    if running:
+        screen.blit(action, (frog_rect.x, frog_rect.y))
 
     ##### movement function here #####
     if moving_left == True:
@@ -113,22 +123,39 @@ while running:
         velocity[0] += 2
 
     for x in data:
-        rect = pygame.Rect(x[0], x[1], rock_img.get_width(), rock_img.get_height())
-        screen.blit(rock_img, (rect.x, rect.y))
-        x[0] += x[2][0]
-        x[1] += x[2][1]
+        if running:
+            rect = pygame.Rect(x[0], x[1], rock_img.get_width(), rock_img.get_height())
+            screen.blit(rock_img, (x[0], x[1]))
 
-        if rect.colliderect(top_rect) or rect.colliderect(bottom_rect) or rect.colliderect(left_rect) or rect.colliderect(right_rect):
-            '''
-            try:
-                data.remove(data[x[3]])
-            except:
-                print(data)
-                print(x)
-            '''
-            print(rock_index)
-            new_rock = append_rocks()
-            data.append(new_rock)
+            #if (rect.x < 0) or (rect.x > screen_width) or (rect.y < 0) or (rect.y > screen_height):
+
+            if rect.colliderect(bottom_rect) or rect.colliderect(top_rect) or rect.colliderect(left_rect) or rect.colliderect(right_rect):
+                ran_x, ran_y, x_vel, y_vel = generate_new()
+                x[0] = ran_x
+                x[1] = ran_y
+                x[2] = x_vel
+                x[3] = y_vel
+                ran_x, ran_y, x_vel, y_vel = generate_new()
+
+                #Unhash the line below if you want to see shit go down
+                #data.append([ran_x, ran_y, x_vel, y_vel])
+
+
+            if (rect.x > 0) or (rect.x < screen_width) or (rect.y > 0) or (rect.y < screen_height):
+                x[0] += int(x[2])
+                x[1] += int(x[3])
+
+        if frog_rect.colliderect(rect):
+            fill = (0, 0, 0)
+            running = False
+            ### blit ending screen ###
+            game_over = True
+
+    if game_over:
+        screen.blit(d_img, (0, 250))
+        screen.blit(e_img, (100, 250))
+        screen.blit(a_img, (200, 250))
+        screen.blit(d_img, (300, 250))
 
     for event in pygame.event.get():
         if event.type == QUIT:
